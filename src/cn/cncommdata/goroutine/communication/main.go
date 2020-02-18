f183faf7b920d99790a3793a019409a06ce67cf3
niulibing <libing_niu@163.com>
2020-02-18 11:54:36 +0800

    优化协程之间的通信日志

--- src/cn/cncommdata/goroutine/communication/main.go
+++ src/cn/cncommdata/goroutine/communication/main.go
@@ -6,11 +6,12 @@ import (
 	"io"
 	"os"
 	"runtime"
+	"strings"
 	"time"
 )
 
 /**
- * 日志监控系统
+ * 日志监控系统「包括日志的读取、解析、写入三个模块，同时工作」
  * @author:libing.niu
  * @date: 2020/2/17 14:47
  */
@@ -63,7 +64,9 @@ func (r *ReadFromFile) Read(rc chan string) {
 			panic(fmt.Sprintf("ReadString error:%s", err.Error()))
 		}
 		//将读取到的数据发送至rc通道，实现读取模块和解析模块之间的数据通信
-		rc <- readString
+
+		rc <- readString[:len(readString)-1]
+		fmt.Printf("数据读取模块从日志文件中读取到的数据:【%v】发送到rc通道。", readString)
 	}
 }
 
@@ -71,22 +74,27 @@ func (r *ReadFromFile) Read(rc chan string) {
 func (w *WriteToFluxDB) Write(wc chan string) {
 
 	//写入模块中从通道wc中获取数据，进行数据处理。在此只做了数据打印
+	fmt.Println("数据解析模块开始从wc通道中获取数据")
 	for s := range wc {
-
+		fmt.Printf("数据解析模块从wc通道中获取数据为:%v\n", s)
 		fmt.Println(s)
 	}
 }
 func (l *LogProcess) Process() {
 
 	//解析模块，从rc通道中解析数据，并且将解析出来的数据发送至wc通道与写入模块进行数据通信
+	fmt.Println("数据解析模块开始从rc通道中获取数据")
 	for s := range l.rc {
-		l.wc <- s
+		fmt.Printf("数据解析模块从rc通道中获取数据为:%v\n", s)
+		upper := strings.ToUpper(s)
+		fmt.Printf("数据解析模块对rc通道中获取数据进行处理，处理后的数据为:%v并且发送到wc通道\n", upper)
+		l.wc <- upper
 	}
 }
 func main() {
 
 	//查看该电脑cpu核数
-	fmt.Printf("该电脑cpu为【%d】核...", runtime.NumCPU())
+	fmt.Printf("该电脑cpu为【%d】核...\n", runtime.NumCPU())
 	//设置cpu的调度个数，如果不设置，默认调用全部cpu核数。
 	runtime.GOMAXPROCS(runtime.NumCPU() - 1)
 
@@ -102,8 +110,11 @@ func main() {
 	}
 	//启动三个协程
 	go lp.read.Read(lp.rc)
+	fmt.Println("读取协程启动完成")
 	go lp.Process()
+	fmt.Println("解析协程启动完成")
 	go lp.write.Write(lp.wc)
+	fmt.Println("写入协程启动完成")
 	//主程序延时关闭
 	time.Sleep(120 * time.Second)
 
---
+++ src/cn/cncommdata/goroutine/synchronization/main.go
@@ -0,0 +1,15 @@
+package main
+
+/**
+
+ * 协程同步 系统工具 sync.waitgroup
+ *      1.Add(delta int) 添加协程记录
+ *      2.Done() 移除协程记录
+ *      3.Wait()同步等待所有记录的协程全部结束
+ *
+ * @author:libing.niu
+ * @date: 2020/2/18 11:19
+ */
+func main() {
+
+}
